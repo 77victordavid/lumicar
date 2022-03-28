b124b771107584cc50fadbd2e57882acd3b292cd
Victor David <77victordavid@gmail.com>
2022-03-27 21:24:59 -0500

    Agregada maquetacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
+import { ContactComponent } from './pages/contact/contact.component';
+import { CheckOutComponent } from './pages/check-out/check-out.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  {    
+    path: 'home', component: HomeComponent
+  },
+  {    
+    path: 'products', component: ProductsComponent
+  },
+  {    
+    path: 'check-out', component: CheckOutComponent
+  },
+  {    
+    path: 'contact', component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CheckOutComponent } from './pages/check-out/check-out.component';
+import { LayoutComponent } from './layout/layout.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckOutComponent,
+    LayoutComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
