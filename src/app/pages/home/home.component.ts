db148fcd2efdb1e7164e0da7c259f26e300011d3
Victor David <77victordavid@gmail.com>
2022-03-28 22:16:51 -0500

    Cambio de textos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  images = [
+    'https://images.philips.com/is/image/PhilipsConsumer/11972UE2X2-IMS-es_CO?$jpglarge$&wid=600', 
+    'https://images.philips.com/is/image/PhilipsConsumer/11972U50CWX2_40-IMS-es_CO?$jpglarge$&wid=600',
+    'https://images.philips.com/is/image/PhilipsConsumer/18952C2-IMS-es_CO?$jpglarge$&wid=600'
+  ];
+  showNavigationArrows = true;
+  showNavigationIndicators = true;
+  
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+  }
 
   ngOnInit(): void {
     
